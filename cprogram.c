e75251c99ab1e0c92bcd7c8ac33712e33d12ac1d
Ubuntu <ubuntu@ip-172-31-23-68.ec2.internal>
2019-02-19 08:23:05 +0000

    Pushing files to Github

---
+++ cprogram.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+printf("Hello this is C program. thank you!");
+return 0;
+}
+
---
+++ javaprogram.java
@@ -0,0 +1,6 @@
+class javaprogram {
+public static void main (String args[]{
+System.out.println("this is java program. Thank you!");
+}
+}
+
